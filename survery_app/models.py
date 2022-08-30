43e06e1441425cc9e49926dede54a2fa17bf601d
cheesenaan <70636155+cheesenaan@users.noreply.github.com>
2022-08-30 07:26:41 -0400

    Update models.py

--- survery_app/models.py
+++ survery_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # DELETE FROM survery_app_question; DELETE FROM SQLite_sequence WHERE name='survery_app_question';
 
+
 class question(models.Model):
     id = models.AutoField(primary_key=True)
     group_مجموعة = models.CharField(max_length=256, choices=[('Affinity', 'Affinity'), ('Collection of Information' , 'Collection of Information'), ('Make decison', 'Make decison') , ('Time spending' , 'Time spending') ])
@@ -10,11 +11,12 @@ class question(models.Model):
     question_english_سؤال_الانجليزي = models.CharField(max_length=500)
     question_arabic_سؤال_عربي = models.CharField(max_length=500)
 
-    title_of_answer_choice_one_عنوان_الإجابة_الأول= models.CharField(max_length=500)
+    title_of_answer_choice_one_عنوان_الإجابة_الأول= models.CharField(max_length=256, choices=[('Extrovert', 'Extrovert'), ('Sensing' , 'Sensing'), ('Thinking', 'Thinking') , ('Judging' , 'Judging') ])
+
     asnwer_choice_one_english_الإجابة_الأولى_انجليزي = models.CharField(max_length=500)
     asnwer_choice_one_arabic_الإجابة_الأولى_عربى = models.CharField(max_length=500)
 
-    title_of_answer_choice_two_عنوان_الإجابة_الثاني = models.CharField(max_length=500)
+    title_of_answer_choice_two_عنوان_الإجابة_الثاني = models.CharField(max_length=256, choices=[('Introvert', 'Introvert'), ('Intuition' , 'Intuition'), ('Feeling', 'Feeling') , ('Perceiving' , 'Perceiving') ])
     asnwer_choice_two_english_الإجابة_الثاني_انجليزي = models.CharField(max_length=500)
     asnwer_choice_two_arabic_الإجابة_الثاني_عربى = models.CharField(max_length=500)
 
